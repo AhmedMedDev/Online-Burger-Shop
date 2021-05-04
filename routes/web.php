37003c53d7e4129c0a05fdc0c962c09628caa9dd
dedoa82 <67242566+dedoa82@users.noreply.github.com>
2021-05-04 05:38:38 -0700

    Improv_Routes

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::view('/', 'welcome');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-
 Route::resource('product', 'ProductController');
 Route::resource('cart', 'CartController');
 Route::resource('checkOut', 'CheckController');
-
-Route::view('Done', 'done');
 
 Route::post('/billing', 'BillingController@placeOrder');
 
