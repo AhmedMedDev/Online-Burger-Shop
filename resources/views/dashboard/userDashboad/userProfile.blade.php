5e11ca80e99aa897746416e0193956604632c34c
dedoa82 <67242566+dedoa82@users.noreply.github.com>
2021-05-15 00:38:58 -0700

    User-Profile-Back

--- resources/views/dashboard/userDashboad/userProfile.blade.php
+++ resources/views/dashboard/userDashboad/userProfile.blade.php
@@ -11,15 +11,13 @@
                 <div class="card card-profile">
                   <div class="card-avatar">
                     <a href="javascript:;">
-                      <img class="img" src="{{asset('images/upload/userAvatar/'.Auth::user()->img)}}" />
+                      <img class="img" src="{{asset('images/upload/userAvatar/'. Auth::user()->img)}}" />
                     </a>
                   </div>
                   <div class="card-body">
-                    <h6 class="card-category text-gray">CEO / Co-Founder</h6>
-                    <h4 class="card-title">Alec Thompson</h4>
-                    <p class="card-description">
-                      Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
-                    </p>
+                    <h6 class="card-category text-gray">{{Auth::user()->jobTitle}}</h6>
+                    <h4 class="card-title">{{Auth::user()->Fname}} {{Auth::user()->Lname}}</h4>
+                    <p class="card-description">  {{Auth::user()->bio}}  </p>
                     <a href="javascript:;" class="btn btn-primary btn-round">Follow</a>
                   </div>
                 </div>
