e6e1e5e5b215f207c51c13db5483bd9e37d50e60
dedoa82 <67242566+dedoa82@users.noreply.github.com>
2021-05-15 08:21:47 -0700

    Front-Edit-Profile

--- resources/views/dashboard/userDashboad/editProfile.blade.php
+++ resources/views/dashboard/userDashboad/editProfile.blade.php
@@ -4,117 +4,72 @@
 
 @section('mainContent')
         {{--  --}}
-          <div class="content">
-            <div class="container-fluid">
-              <div class="row">
-                <div class="col-md-10">
-                  <div class="card">
-                    <div class="card-header card-header-primary">
-                      <h4 class="card-title">Edit Profile</h4>
-                      <p class="card-category">Complete your profile</p>
-                    </div>
-                    <div class="card-body">
-                      <form>
-                        <div class="row">
-                          <div class="col-md-5">
-                            <div class="form-group">
-                              <label class="bmd-label-floating">Company (disabled)</label>
-                              <input type="text" class="form-control" disabled>
-                            </div>
-                          </div>
-                          <div class="col-md-3">
-                            <div class="form-group">
-                              <label class="bmd-label-floating">Username</label>
-                              <input type="text" class="form-control">
-                            </div>
-                          </div>
-                          <div class="col-md-4">
-                            <div class="form-group">
-                              <label class="bmd-label-floating">Email address</label>
-                              <input type="email" class="form-control">
-                            </div>
-                          </div>
-                        </div>
-                        <div class="row">
-                          <div class="col-md-6">
-                            <div class="form-group">
-                              <label class="bmd-label-floating">Fist Name</label>
-                              <input type="text" class="form-control">
-                            </div>
-                          </div>
-                          <div class="col-md-6">
-                            <div class="form-group">
-                              <label class="bmd-label-floating">Last Name</label>
-                              <input type="text" class="form-control">
-                            </div>
-                          </div>
-                        </div>
-                        <div class="row">
-                          <div class="col-md-12">
-                            <div class="form-group">
-                              <label class="bmd-label-floating">Adress</label>
-                              <input type="text" class="form-control">
-                            </div>
-                          </div>
-                        </div>
-                        <div class="row">
-                          <div class="col-md-4">
-                            <div class="form-group">
-                              <label class="bmd-label-floating">City</label>
-                              <input type="text" class="form-control">
-                            </div>
-                          </div>
-                          <div class="col-md-4">
-                            <div class="form-group">
-                              <label class="bmd-label-floating">Country</label>
-                              <input type="text" class="form-control">
-                            </div>
-                          </div>
-                          <div class="col-md-4">
-                            <div class="form-group">
-                              <label class="bmd-label-floating">Postal Code</label>
-                              <input type="text" class="form-control">
-                            </div>
-                          </div>
-                        </div>
-                        <div class="row">
-                          <div class="col-md-12">
-                            <div class="form-group">
-                              <label>About Me</label>
+        <div class="content address">
+          <div class="container-fluid">
+            <div class="row justify-content-center">
+              <div class="col-md-10">
+                <div class="card">
+                  <div class="card-header card-header-primary">
+                    <h4 class="card-title">Update Profile</h4>
+                    <p class="card-category">Edit Your Profile Info</p>
+                  </div>
+                  {{--  --}}
+                  <div class="card-body">
+                    <form id="AddAddress" enctype="multipart/form-data"> 
+                      <div class="row justify-content-center">
+                          <div class="col-md-10">
+
                               <div class="form-group">
-                                <label class="bmd-label-floating"> Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.</label>
-                                <textarea class="form-control" rows="5"></textarea>
+                                  <label class="bmd-label-floating">Fist Name </label>
+                                  <input type="text" class="form-control" id="Fname" name="name" value="{{ Auth::user()->Fname }}">
                               </div>
-                            </div>
+
+                              <div class="form-group">
+                                  <label class="bmd-label-floating">Last Name</label>
+                                  <input type="text" class="form-control" id="Lname" name="country" value="{{ Auth::user()->Lname }}">
+                              </div>
+      
+                              <div class="form-group">
+                                  <label class="bmd-label-floating">Job Title</label>
+                                  <input type="text" class="form-control" id="jobTitle" name="street" value="{{ Auth::user()->jobTitle }}">
+                              </div>
+      
+                              <div class="form-group">
+                                <label for="exampleFormControlTextarea1" class="bmd-label-floating">Bio</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" >{{ Auth::user()->bio }}</textarea>
+                              </div>
+                              {{--  --}}
+                              <div class="file-upload">
+                                <div class="image-upload-wrap">
+                                  <input class="file-upload-input" type='file' onchange="readURL(this);" accept="image/*" value="{{ Auth::user()->img }}"/>
+                                  <div class="drag-text">
+                                    <i class="fa fa-download" aria-hidden="true"></i>
+                                    <h3>Drag and drop a Image </h3>
+                                  </div>
+                                </div>
+                                <div class="file-upload-content">
+                                  <img class="file-upload-image" src="#" alt="your image" />
+                                  <div class="image-title-wrap">
+                                    <button type="button" onclick="removeUpload()" class="remove-image">Remove <span class="image-title">Uploaded Image</span></button>
+                                  </div>
+                                </div>
+                              </div>
+                              {{--  --}}
+                              <input type="text" class="hidden" id="user_id" name="user_id" value="{{Auth::user()->id}}">
                           </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary pull-right">Update Profile</button>
-                        <div class="clearfix"></div>
-                      </form>
-                    </div>
+                      </div>
+                      <div class="clearfix"></div>
+                    </form>
+                    <button type="submit" class="btn btn-primary pull-right" onclick="AddAddreass()">Add Address</button>
+
                   </div>
-                </div>
-                <div class="col-md-4">
-                  <div class="card card-profile">
-                    <div class="card-avatar">
-                      <a href="javascript:;">
-                        <img class="img" src="{{asset('dashboard/assets/img/faces/marc.jpg')}}" />
-                      </a>
-                    </div>
-                    <div class="card-body">
-                      <h6 class="card-category text-gray">CEO / Co-Founder</h6>
-                      <h4 class="card-title">Alec Thompson</h4>
-                      <p class="card-description">
-                        Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
-                      </p>
-                      <a href="javascript:;" class="btn btn-primary btn-round">Follow</a>
-                    </div>
-                  </div>
+                  {{--  --}}
                 </div>
               </div>
             </div>
           </div>
-        {{--  --}}
+        </div>
+      {{--  --}}
 @endsection
         
       
